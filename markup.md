5a5fcd3e2c5fb043f6ea4c8199e21169da0e3f7d
pavelpz <pzika@langmaster.cz>
2022-02-22 12:23:04 +0100

    fix

--- markup.md
+++ markup.md
@@ -1,3 +1,4 @@
 # Riverpod navigator examples
 
 ### [simple](riverpod_navigator_example/index.html)
+### [doc_async](doc_async/index.html)
---
+++ riverpod_navigator_example/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import 'src/widgets.dart';
+
+void main() {
+  runApp(ProviderScope(child: const AppRoot()));
+}
+
+// flutter build web --web-renderer html
