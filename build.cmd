c72c658454659ced39e5a4a53831d07ae13c4c36
pavelpz <pzika@langmaster.cz>
2022-03-03 12:43:37 -0100

    wait animation

--- build.cmd
+++ build.cmd
@@ -1,17 +1,18 @@
+set proc=c:\pavelpz.github.io\buildProc.cmd
 rem %0 \examples\riverpod_navigator_example\
 rem %1 /riverpod_navigator_example/
 rem %2 \riverpod_navigator_example\
 
 rem call D:\pavelpz.github.io\buildProc.cmd \examples\riverpod_navigator_example\ /riverpod_navigator_example/ \riverpod_navigator_example\ main
 
-call D:\pavelpz.github.io\buildProc.cmd \examples\doc\ /doc_simple/ \doc_simple/\ simple
+call %proc% \examples\doc\ /doc_simple/ \doc_simple/\ simple
 
-call D:\pavelpz.github.io\buildProc.cmd \examples\doc\ /doc_async/ \doc_async/\ async
+call %proc% \examples\doc\ /doc_async/ \doc_async/\ async
 
-call D:\pavelpz.github.io\buildProc.cmd \examples\doc\ /doc_login_flow/ \doc_login_flow/\ login_flow
+call %proc% \examples\doc\ /doc_login_flow/ \doc_login_flow/\ login_flow
 
-call D:\pavelpz.github.io\buildProc.cmd \examples\doc\ /doc_nested_navigation/ \doc_nested_navigation/\ nested_navigation
+call %proc% \examples\doc\ /doc_nested_navigation/ \doc_nested_navigation/\ nested_navigation
 
-call D:\pavelpz.github.io\buildProc.cmd \examples\doc\ /medium_main/ \medium_main/\ medium_main
+call %proc% \examples\doc\ /medium_main/ \medium_main/\ medium_main
 
-call D:\pavelpz.github.io\buildProc.cmd \examples\doc\ /medium_more_providers/ \medium_more_providers/\ medium_more_providers
+call %proc% \examples\doc\ /medium_more_providers/ \medium_more_providers/\ medium_more_providers
--- buildProc.cmd
+++ buildProc.cmd
@@ -1,6 +1,7 @@
+set drive=c
 cd \riverpod_navigator%1
 call flutter build web --web-renderer html --base-href %2 -t lib\%4.dart
-RD /S /Q d:\pavelpz.github.io%2
-MD d:\pavelpz.github.io%2
-RD /S /Q d:\riverpod_navigator%1build\web\canvaskit
-robocopy d:\riverpod_navigator%1build\web\ d:\pavelpz.github.io%3 /E /MOV
+RD /S /Q %drive%:\pavelpz.github.io%2
+MD %drive%:\pavelpz.github.io%2
+RD /S /Q %drive%:\riverpod_navigator%1build\web\canvaskit
+robocopy %drive%:\riverpod_navigator%1build\web\ %drive%:\pavelpz.github.io%3 /E /MOV
